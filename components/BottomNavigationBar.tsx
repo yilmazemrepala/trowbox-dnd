eeefad0b8c861fb8e8d23e2603182dd1b6e14b85
Yilmaz <yilmaz@babel.com.tr>
2025-03-10 09:44:11 +0300

    .asd

--- components/BottomNavigationBar.tsx
+++ components/BottomNavigationBar.tsx
@@ -7,11 +7,11 @@ import { Link, Quote, Image, Laptop, Smartphone } from "lucide-react";
 function BottomNavigationBar() {
 	return (
 		<div className="fixed bottom-10 left-0 right-0 z-50 flex justify-center items-center">
-			<div className="bg-white border border-gray-200 rounded-full w-fit h-16 flex px-4 items-center">
+			<div className="bg-white border border-gray-200 rounded-2xl w-fit h-14 flex px-4 items-center">
 				<div className="flex flex-row gap-4 items-center">
 					<Button
 						variant="outline"
-						className="bg-green-400 hover:bg-green-400 text-white hover:text-white font-bold hover:border-green-500 transition-all duration-75 relative overflow-hidden shimmer-button rounded-full px-6">
+						className="bg-green-400 hover:bg-green-400 text-white hover:text-white font-bold hover:border-green-500 transition-all duration-75 relative overflow-hidden shimmer-button rounded-lg px-6">
 						<span className="relative z-10">Share my Bento</span>
 					</Button>
 
